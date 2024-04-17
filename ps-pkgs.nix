0216ddf91bbf77b9f709e2e4e60e275359042e46
mariusgeorgescu <georgescumarius@live.com>
2024-04-17 17:17:32 +0300

    Update ps-pkgs.nix

--- ps-pkgs.nix
+++ ps-pkgs.nix
@@ -82,12 +82,10 @@ with self; {
 
   cardano-serialization-lib = {
     src.git = {
-      repo = "https://github.com/mlabs-haskell/purescript-cardano-serialization-lib/";
+      repo = "https://github.com/mlabs-haskell/purescript-cardano-serialization-lib";
       rev = "da2195fecac5442851dbc3ac3b6e1d805ced6c05";
     };
-    info = {
-      version = "0.0.0";
-    };
+    info = {};
   };
   
   cardano-transaction-lib = {
